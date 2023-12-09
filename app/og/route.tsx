9f88c213ad1ebb1bc81a42153a8b239a4223b835
Rand0mLetters <boris.nezlobin@gmail.com>
2023-12-09 15:58:51 -0800

    analytics + fixes

--- app/og/route.tsx
+++ app/og/route.tsx
@@ -11,12 +11,12 @@ export async function GET(request: NextRequest) {
     return new ImageResponse((
         <div tw="w-full flex h-full bg-[#f5f5f5]">
             <div tw="mx-auto flex flex-col justify-center items-start">
-                <p tw='text-base text-[#707070]'>
+                <p tw='text-lg text-[#707070]'>
                     {params.get("info")}
                 </p>
                 <div tw="flex flex-col justify-center items-end">
-                    <p tw="text-6xl font-bold text-black m-0">{params.get("title") || "Boris Nezlobin"}</p>
-                    <p tw='text-base text-[#707070]'>
+                    <p tw="text-8xl font-bold text-black m-0">{params.get("title") || "Boris Nezlobin"}</p>
+                    <p tw='text-lg text-[#707070]'>
                         {subtitle}{subtitle ? " / " : ""}Boris Nezlobin
                     </p>
                 </div>
