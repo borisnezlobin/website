ce1f46d3346f1a3ce4a93ea58d7f3e3b675c0881
Rand0mLetters <boris.nezlobin@gmail.com>
2023-12-18 20:24:46 -0800

    add /projects and new font

---
+++ app/projects/page.tsx
@@ -0,0 +1,13 @@
+export default function ProjectsPage() {
+  return (
+    <main className="p-4">
+      <h1 className="text-6xl font-bold text-left dark:text-dark edo">
+        Projects
+      </h1>
+      <p className="dark:text-dark text-left mt-2">
+        Check out all of the things I've worked on!<br />
+        I've made a lot of cool stuff, and helped out with some open-source projects that I'm interested in. For every project, I tend to do a writeup on it, so you can learn more about it and how it works. They also serve as a way for me to document what I've learned and practice my writing skills.
+      </p>
+    </main>
+  )
+}
