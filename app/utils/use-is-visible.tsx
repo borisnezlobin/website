fa770db311aff05af07dcdedb3cb1aa7957095ae
borisnezlobin <me@borisnezlobin.com>
2024-06-30 10:37:55 -0700

    how did I forget condiional hook calling

--- app/utils/use-is-visible.tsx
+++ app/utils/use-is-visible.tsx
@@ -3,17 +3,15 @@ import { RefObject, useEffect, useState } from "react";
 export function useIsVisible(ref: RefObject<HTMLElement>) {
     const [isIntersecting, setIntersecting] = useState(false);
 
-    if (ref == null) return false;
-
     useEffect(() => {
-        if(ref.current == null) return;
+        if(ref == null || ref.current == null) return;
 
         const observer = new IntersectionObserver(([entry]) => {
             setIntersecting(entry.isIntersecting);
         });
 
         observer.observe(ref.current);
-        
+
         return () => {
             observer.disconnect();
         };
