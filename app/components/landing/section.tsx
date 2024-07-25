44ced43a1926c45c8c997d875fc5e30bea2a50ea
borisnezlobin <boris.nezlobin@gmail.com>
2024-07-25 11:14:35 -0700

    fix window is not defined (lol) and fix color transition on home page

--- app/components/landing/section.tsx
+++ app/components/landing/section.tsx
@@ -3,6 +3,9 @@
 import { useIsVisible } from "@/app/utils/use-is-visible";
 import { useRef } from "react";
 
+// todo: find some animation/moving thingy other than
+// [transition:all_300ms,opacity_2s,transform_2s] ${isVisible ? "opacity-100 translate-y-0 scale-100" : "opacity-0 translate-y-4 scale-105"}
+
 const Section = ({
     title, description, children
 }: { title: string, description: string, children: any }) => {
@@ -10,11 +13,11 @@ const Section = ({
     const isVisible = useIsVisible(ref);
 
     return (
-        <div ref={ref} className={`w-full flex flex-col justify-center items-left mt-4 [transition:all_300ms,opacity_2s,transform_2s] ${isVisible ? "opacity-100 translate-y-0 scale-100" : "opacity-0 translate-y-4 scale-105"}`}>
-            <h2 className="text-xl sm:text-4xl mt-12 font-bold text-left dark:text-dark">
+        <div ref={ref} className={`w-full flex flex-col justify-center items-left mt-4`}>
+            <h2 className="text-xl sm:text-4xl mt-12 font-bold text-left dark:text-dark transition-colors duration-300">
                 {title}
             </h2>
-            <p className="mt-4">
+            <p className="mt-4 transition-colors duration-300">
                 {description}
             </p>
             {children}
