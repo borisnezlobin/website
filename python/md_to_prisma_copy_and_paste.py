626c8625f543b7447bdadfe3e9a9d3c62679f0cc
borisnezlobin <me@borisnezlobin.com>
2024-07-25 19:55:33 -0700

    finally get around to making a script to copy-paste markdown files into prisma studio

---
+++ python/md_to_prisma_copy_and_paste.py
@@ -0,0 +1,19 @@
+# this file takes a file path as an argument and copies the contents of the file to the clipboard
+# except each newline is replace with a \\n so that you can copy and paste the contents into prisma studio
+
+import sys
+import pyperclip
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: python3 md_to_prisma_copy_and_paste.py <file_path>")
+        sys.exit(1)
+    file_path = sys.argv[1]
+    with open(file_path, 'r') as file:
+        file_contents = file.read()
+    file_contents = file_contents.replace('\n', '\\n')
+    pyperclip.copy(file_contents)
+    print("File contents copied to clipboard")
+
+if __name__ == "__main__":
+    main()
