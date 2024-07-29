8e5cf634ba0ba9276dfb6f0a552d254b8e39d1fe
borisnezlobin <me@borisnezlobin.com>
2024-07-28 17:35:40 -0700

    clean up article-body.tsx

--- app/components/article-body.tsx
+++ app/components/article-body.tsx
@@ -7,23 +7,13 @@ const ArticleBody = ({ body }: { body: string}) => {
     const parseBody = (body: string) => {
         let formattedBody = body.replaceAll("NEWLINE", "\n");
 
-
-        // replace all instances of `<` that aren't followed by a name in `components` with `&lt;`
-        // this is to prevent React from rendering the tags
-
-        formattedBody = formattedBody.replace(/<a href="(https:\/\/t\.co\/[a-zA-Z0-9]+)">pic\.twitter\.com\/[a-zA-Z0-9]+<\/a>/g, (match, url) => {
-            return `<img src="${url}" alt="Twitter Image" />`;
-        });
-            
-
-
         // if we encounter $something$, we want to replace it with
         // <MathEmbed>{'something'}</MathEmbed>
         // if it's $$something$$, we want to replace it with
         // <MathEmbed display={true}>{'something'}</MathEmbed>
 
         formattedBody = formattedBody.replace(/\$\$([^$]+)\$\$/g, (_, match) => {
-            let replaced = match.replaceAll("\n", ""); // THIS IS IMPORTANT (and silly)
+            let replaced = match.replaceAll("\n", ""); // THIS IS IMPORTANT (and silly) to stop the newlines from creating `p` tags
             replaced = replaced.replaceAll("\\", "\\\\");
             replaced = replaced.replaceAll("{", "\\{");
             return `<MathEmbed display={true}>${replaced}</MathEmbed>`;
@@ -39,7 +29,6 @@ const ArticleBody = ({ body }: { body: string}) => {
     }
 
     return <article>
-        {/* <Script src="https://platform.twitter.com/widgets.js" /> */}
         <MDXRemote source={`
             ${parseBody(body)}
         `} components={{ GistEmbed, MathEmbed, Tweet }} />
