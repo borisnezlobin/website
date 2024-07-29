cb11b160aab45b9ea0fca151157739d7fe82ce8a
borisnezlobin <me@borisnezlobin.com>
2024-07-29 09:56:50 -0700

    move imports away from root layout, add footnote embed

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,19 +3,10 @@ import Providers from "./_contexts/providers";
 import getMetadata from "./lib/metadata";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Analytics } from "@vercel/analytics/react";
-import "./styles/light.css";
-import "./styles/dark.css";
-import "./styles/gist.css";
-import Script from "next/script";
 import Footer from "./components/footer";
 import Link from "next/link";
 
 export const metadata = getMetadata({});
-// {
-//   title: 'Boris Nezlobin',
-//   description:
-//     "I'm a software engineer, full-stack developer, and computer science student. I'm passionate about building things that make people's lives easier.",
-// }
 
 export default function RootLayout({
   children,
@@ -25,17 +16,8 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-      <link
-          rel="stylesheet"
-          href="https://cdn.jsdelivr.net/npm/katex@0.13.11/dist/katex.min.css"
-          integrity="sha384-Um5gpz1odJg5Z4HAmzPtgZKdTBHZdw8S29IecapCSB31ligYPhHQZMIlWLYQGVoc"
-          crossOrigin="anonymous"
-        />
       </head>
       <body suppressHydrationWarning>
-        {/* <script src="/prism.js"></script> */}
-        <Script src="https://unpkg.com/prismjs@v1.x/components/prism-core.min.js"></Script>
-        <Script src="https://unpkg.com/prismjs@v1.x/plugins/autoloader/prism-autoloader.min.js"></Script>
         <Providers>
           <div className="w-full fixed z-10 top-0 left-0 p-4">
             <div className="flex flex-row justify-around w-full sm:w-1/2 items-center">
