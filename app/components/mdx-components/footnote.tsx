cd031fa1ce68f2d5c1e255d1f2a847b9171ac6d6
borisnezlobin <boris.nezlobin@gmail.com>
2024-07-29 16:59:41 -0700

    not the cursed css focus/unfocus bruh

--- app/components/mdx-components/footnote.tsx
+++ app/components/mdx-components/footnote.tsx
@@ -1,11 +1,33 @@
-import { ReactNode } from "react";
+import { X } from "@phosphor-icons/react/dist/ssr";
+import React, { ReactNode } from "react";
 
 const Footnote = ({ id, children }: { id: string, children?: ReactNode }) => {
     return (
-        <span className="group relative z-20">
-            <span className="text-sm top-[-0.5rem] relative font-semibold text-sky-700 cursor-pointer link decoration-transparent">[{id}]</span>
-            <span className="hidden group-hover:block absolute bg-light-background dark:bg-dark-background border border-[#d4d4d4] dark:border-[#525252] left-1/2 -translate-x-1/2 rounded-lg shadow-md dark:shadow-none px-6 py-3 top-3 w-max max-w-2xl">
+        <span className="relative group">
+            <input type="checkbox" id={`footnote-${id}`} className="hidden peer" />
+            <label
+                htmlFor={`footnote-${id}`}
+                className="cursor-pointer text-xl font-bold text-sky-700 link relative z-10"
+            >
+                *
+            </label>
+            <span
+                className="hidden group-hover:block peer-checked:block absolute bg-light-background dark:bg-dark-background 
+                border border-[#d4d4d4] dark:border-[#525252] left-1/2 -translate-x-1/2 rounded-lg shadow-md dark:shadow-none 
+                px-6 py-3 top-3 w-max max-w-[90vw] md:max-w-3xl"
+                style={{
+                    pointerEvents: 'none',
+                    zIndex: 20,
+                }}
+            >
                 {children}
+                <label
+                    htmlFor={`footnote-${id}`}
+                    className="w-full h-12 flex gap-2 flex-row justify-center items-center cursor-pointer text-xl font-bold text-red-600"
+                    style={{ pointerEvents: 'auto' }}
+                >
+                    <X /> Close
+                </label>
             </span>
         </span>
     );
