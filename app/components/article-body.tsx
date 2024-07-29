93f52b03ad16bf6b9b0429fa674300aec8f1bfd8
borisnezlobin <me@borisnezlobin.com>
2024-07-29 09:56:50 -0700

    move imports away from root layout, add footnote embed

--- app/components/article-body.tsx
+++ app/components/article-body.tsx
@@ -2,8 +2,12 @@ import { MDXRemote } from "next-mdx-remote/rsc";
 import { GistEmbed } from "./mdx-components/gist-embed";
 import MathEmbed from "./mdx-components/math-embed";
 import Tweet from "./mdx-components/twitter-embed";
+import Footnote from "./mdx-components/footnote";
+import "../styles/light.css";
+import "../styles/dark.css";
+import "../styles/gist.css";
 
-const ArticleBody = ({ body }: { body: string}) => {
+const ArticleBody = ({ body }: { body: string }) => {
     const parseBody = (body: string) => {
         let formattedBody = body.replaceAll("NEWLINE", "\n");
 
@@ -29,10 +33,25 @@ const ArticleBody = ({ body }: { body: string}) => {
     }
 
     return <article>
+        <link
+            rel="stylesheet"
+            href="https://cdn.jsdelivr.net/npm/katex@0.13.11/dist/katex.min.css"
+            integrity="sha384-Um5gpz1odJg5Z4HAmzPtgZKdTBHZdw8S29IecapCSB31ligYPhHQZMIlWLYQGVoc"
+            crossOrigin="anonymous"
+        />
+        <Script src="https://unpkg.com/prismjs@v1.x/components/prism-core.min.js"></Script>
+        <Script src="https://unpkg.com/prismjs@v1.x/plugins/autoloader/prism-autoloader.min.js"></Script>
         <MDXRemote source={`
             ${parseBody(body)}
-        `} components={{ GistEmbed, MathEmbed, Tweet }} />
+        `} components={{ GistEmbed, MathEmbed, Tweet, Footnote, a: NewTabLink }} />
     </article>
 };
+
+import { AnchorHTMLAttributes } from "react";
+import Script from "next/script";
+
+const NewTabLink: React.FC<AnchorHTMLAttributes<HTMLAnchorElement>> = ({ href, children }) => {
+    return <a href={href} target="_blank" rel="noopener noreferrer">{children}</a>;
+}
 
 export default ArticleBody;
