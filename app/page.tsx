7b6e3b1e42072fc0403c9c164ead6593d4cf6fd3
borisnezlobin <boris.nezlobin@gmail.com>
2024-07-29 18:01:32 -0700

    I WAS SUPPOSED TO BE USING SVH BRUH

--- app/page.tsx
+++ app/page.tsx
@@ -7,95 +7,96 @@ import { Section } from "./components/landing/section";
 import { LandingPageBadge } from "./components/landing/landing-page-badge";
 
 const skills = [
-  { title: "TypeScript", description: "My go-to language for web projects" },
-  { title: "Java", description: "Used for robotics and AP CSA" },
-  { title: "C++", description: "Used for competitive programming (USACO)" },
-  { title: "Python", description: "First language I learned" },
-  { title: "C#", description: "Game dev in Unity" },
-  { title: "Rust", description: "Oh crab..." },
-  { title: "JavaScript", description: "" },
-  { title: "HTML/CSS", description: "" },
-  { title: "Git", description: "Learned by contributing to open source" },
-  { title: "Docker", description: "" },
-  { title: "Postman", description: "APIs never work on the first try" },
-  { title: "React", description: "Used for OneShip's frontend" },
-  { title: "React Native", description: "The only way I know how to make mobile apps" },
-  { title: "NextJS", description: "Powering this website" },
-  { title: "TailwindCSS", description: "text-center p-4 font-bold" },
-  { title: "Prisma + Postgres", description: "How else would I store my blogs?" },
-  { title: "ElectronJS", description: "Easy desktop apps" },
-  { title: "ExpressJS", description: "" },
-  { title: "IDEs", description: "VSCode, CLion, IntelliJ, AS, and Zed are my favorites"},
-  { title: "Team Management", description: "Software Lead for Kuriosity Robotics, '24-25" },
+    { title: "TypeScript", description: "My go-to language for web projects" },
+    { title: "Java", description: "Used for robotics and AP CSA" },
+    { title: "C++", description: "Used for competitive programming (USACO)" },
+    { title: "Python", description: "First language I learned" },
+    { title: "C#", description: "Game dev in Unity" },
+    { title: "Rust", description: "Oh crab..." },
+    { title: "JavaScript", description: "" },
+    { title: "HTML/CSS", description: "" },
+    { title: "Git", description: "Learned by contributing to open source" },
+    { title: "Docker", description: "" },
+    { title: "Postman", description: "APIs never work on the first try" },
+    { title: "React", description: "Used for OneShip's frontend" },
+    { title: "React Native", description: "The only way I know how to make mobile apps" },
+    { title: "NextJS", description: "Powering this website" },
+    { title: "TailwindCSS", description: "text-center p-4 font-bold" },
+    { title: "Prisma + Postgres", description: "How else would I store my blogs?" },
+    { title: "ElectronJS", description: "Easy desktop apps" },
+    { title: "ExpressJS", description: "" },
+    { title: "IDEs", description: "VSCode, CLion, IntelliJ, AS, and Zed are my favorites" },
+    { title: "Team Management", description: "Software Lead for Kuriosity Robotics, '24-25" },
 ];
 
 const projects = [
-  { title: "OneShip", description: "A full-stack web and mobile application built for my school.", url: "https://paly.app" },
-  { title: "Portfolio", description: "My very own website :) Designed and built from the ground up by me.", url: "https://bnezlobin.vercel.app" },
-  // { title: "Run4U", description: "A work-in-progress, full stack, run generating application for the 2024 Congressional App Challenge" },
-  { title: "YAPA", description: "Yet Another (Electron) Pomodoro App (with nice UI). It has Discord RPC!", url: "https://github.com/borisnezlobin/pomodoro" },
-  { title: "SpatOS", description: "WIP - Yikes! High schoolers trying to make spatial operating systems doesn't turn out well." },
-  { title: "UndoDB", description: "A small, no-SQL, in-memory, transaction-based database written in Java.", url: "https://github.com/borisnezlobin/undodb" },
-  { title: "L!m", description: "A Mozilla and Chrome RegEx-based time limit extension.", url: "https://github.com/borisnezlobin/lim" }
+    { title: "OneShip", description: "A full-stack web and mobile application built for my school.", url: "https://paly.app" },
+    { title: "Portfolio", description: "My very own website :) Designed and built from the ground up by me.", url: "https://bnezlobin.vercel.app" },
+    // { title: "Run4U", description: "A work-in-progress, full stack, run generating application for the 2024 Congressional App Challenge" },
+    { title: "YAPA", description: "Yet Another (Electron) Pomodoro App (with nice UI). It has Discord RPC!", url: "https://github.com/borisnezlobin/pomodoro" },
+    { title: "SpatOS", description: "WIP - Yikes! High schoolers trying to make spatial operating systems doesn't turn out well." },
+    { title: "UndoDB", description: "A small, no-SQL, in-memory, transaction-based database written in Java.", url: "https://github.com/borisnezlobin/undodb" },
+    { title: "L!m", description: "A Mozilla and Chrome RegEx-based time limit extension.", url: "https://github.com/borisnezlobin/lim" }
 ]
 
 const contributions = [
-  { title: "TypeHero", description: "Profanity filter, DevEx, and UI", url: "https://typehero.dev" },
-  { title: "Overlayed", description: "", url: "https://overlayed.dev" },
-  { title: "helloSystem OS", description: "System apps", url: "https://hellosystem.github.io/docs/" },
-  { title: "ShadowFinder", description: "CLI Caching", url: "https://github.com/bellingcat/ShadowFinder" }
+    { title: "TypeHero", description: "Profanity filter, DevEx, and UI", url: "https://typehero.dev" },
+    { title: "Overlayed", description: "", url: "https://overlayed.dev" },
+    { title: "helloSystem OS", description: "System apps", url: "https://hellosystem.github.io/docs/" },
+    { title: "ShadowFinder", description: "CLI Caching", url: "https://github.com/bellingcat/ShadowFinder" }
 ]
 
 export default function Home() {
-  return (
-    <main className="flex flex-col justify-center items-start mb-[30vh] p-4 lg:p-0">
-      <div className="h-[100vh] relative top-[-3rem] items-center w-full flex flex-col justify-center p-4">
-        <p className="text-base md:text-2xl">Hi, I&apos;m</p>
-        <h1 className="text-3xl font-bold dark:text-dark edo text-center md:text-7xl">
-          Boris Nezlobin.
-        </h1>
-      </div>
-      <ScrollForMore />
-      <h2 className="text-xl sm:text-4xl mt-12 font-bold text-left dark:text-dark">
-        <Age />
-      </h2>
-      <div className="w-full flex flex-col md:flex-row justify-center items-center mt-4">
-        <p className="dark:text-dark text-left w-full">
-          I&apos;m an 11th grader at Palo Alto High School writing code for fun.
-          My favorite technologies to work with are T3, Electron, React Native, and Unity.
-          Read on to see my skills, or check out <Link className="link underline font-semibold" href="/blog">my blog</Link>,{" "}
-          <Link className="link underline font-semibold" href="/resume">resume</Link>, or <Link className="link underline font-semibold" href="/projects">projects I&apos;ve worked on</Link>.<br />
-          <span className="w-full flex flex-row justify-start items-center h-full gap-8 mt-4">
-            <LinkButton href="/blog" className="">
-              Check out my blog
-            </LinkButton>
-            <Link className="hidden md:flex link underline flex-row justify-center font-semibold items-center gap-1" href="/resume">
-              Resume
-              <ArrowRight weight="bold" />
-            </Link>
-          </span>
-        </p>
-      </div>
-      <Section
-        id="1.0"
-        title="Skills"
-        description=""
-      >
-        <div className="w-full flex flex-wrap flex-row gap-4">
-          {skills.map((e, i) => <LandingPageBadge title={e.title} description={e.description} key={`skill ${i}`} />)}
-        </div>
-      </Section>
-      <Section
-        id="2.0"
-        title="Projects"
-        description="My favorite projects. Check out /projects for writeups on each!"
-      >
-        <div className="w-full flex flex-wrap flex-row gap-4 mt-4">
-          {projects.map((e, i) => 
-            <LandingPageBadge titleClassName={"w-24"} title={e.title} description={e.description} key={`project ${i}`} url={e.url} />
-          )}
-        </div>
-      </Section>
-    </main>
-  );
+    return (
+        <main className="flex flex-col justify-center items-start mb-[30vh] p-4 lg:p-0">
+
+            <div className="h-[100svh] relative top-[-3rem] items-center w-full flex flex-col justify-center p-4">
+                <p className="text-base md:text-2xl">Hi, I&apos;m</p>
+                <h1 className="text-3xl font-bold dark:text-dark edo text-center md:text-7xl">
+                    Boris Nezlobin.
+                </h1>
+            </div>
+            <ScrollForMore />
+            <h2 className="text-xl sm:text-4xl mt-12 font-bold text-left dark:text-dark">
+                <Age />
+            </h2>
+            <div className="w-full flex flex-col md:flex-row justify-center items-center mt-4">
+                <p className="dark:text-dark text-left w-full">
+                    I&apos;m an 11th grader at Palo Alto High School writing code for fun.
+                    My favorite technologies to work with are T3, Electron, React Native, and Unity.
+                    Read on to see my skills, or check out <Link className="link underline font-semibold" href="/blog">my blog</Link>,{" "}
+                    <Link className="link underline font-semibold" href="/resume">resume</Link>, or <Link className="link underline font-semibold" href="/projects">projects I&apos;ve worked on</Link>.<br />
+                    <span className="w-full flex flex-row justify-start items-center h-full gap-8 mt-4">
+                        <LinkButton href="/blog" className="">
+                            Check out my blog
+                        </LinkButton>
+                        <Link className="hidden md:flex link underline flex-row justify-center font-semibold items-center gap-1" href="/resume">
+                            Resume
+                            <ArrowRight weight="bold" />
+                        </Link>
+                    </span>
+                </p>
+            </div>
+            <Section
+                id="1.0"
+                title="Skills"
+                description=""
+            >
+                <div className="w-full flex flex-wrap flex-row gap-4">
+                    {skills.map((e, i) => <LandingPageBadge title={e.title} description={e.description} key={`skill ${i}`} />)}
+                </div>
+            </Section>
+            <Section
+                id="2.0"
+                title="Projects"
+                description="My favorite projects. Check out /projects for writeups on each!"
+            >
+                <div className="w-full flex flex-wrap flex-row gap-4 mt-4">
+                    {projects.map((e, i) =>
+                        <LandingPageBadge titleClassName={"w-24"} title={e.title} description={e.description} key={`project ${i}`} url={e.url} />
+                    )}
+                </div>
+            </Section>
+        </main>
+    );
 }
