b19244fd82763c36b3df6c6290b3ab79c9fb7c4d
borisnezlobin <boris.nezlobin@gmail.com>
2024-08-07 21:24:37 -0700

    make notes links not scale the border lol

--- app/notes/notes-list-item.tsx
+++ app/notes/notes-list-item.tsx
@@ -3,11 +3,13 @@ import Link from "next/link";
 
 export const NotesListItem = ({ note }: { note: Note }) => {
     return (
-        <Link href={`/notes/${note.slug}`}>
-            <div className="p-4 py-8 hover:scale-105 w-full mt-4 border-y cursor-pointer">
-                <h2 className="text-2xl font-bold">{note.title}</h2>
-                <p>{note.description}</p>
-            </div>
-        </Link>
+        <div className="border-y mt-2 p-4 py-8">
+            <Link href={`/notes/${note.slug}`}>
+                <div className="hover:scale-105 w-full cursor-pointer">
+                    <h2 className="text-2xl font-bold">{note.title}</h2>
+                    <p>{note.description}</p>
+                </div>
+            </Link>
+        </div>
     );
 }
