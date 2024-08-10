1a1a8163ca3bbf4e5c86921f69cd1591157f724a
borisnezlobin <me@borisnezlobin.com>
2024-08-10 09:59:07 -0700

    forgot one page

--- app/blog/components/blog-list-item.tsx
+++ app/blog/components/blog-list-item.tsx
@@ -16,7 +16,7 @@ const BlogListItem = ({
 }) => (
   <div
     key={post.id}
-    className={`${!inGrid ? "mt-8 print:m-0 cursor-pointer group" : "flex flex-shrink-0 relative group cursor-pointer flex-col gap-2 h-48 min-w-64 max-w-[90vw] w-96 p-4 print:p-0 border border-neutral-300 hover:border-neutral-400 dark:border-neutral-600 hover:dark:border-neutral-400 group rounded-lg tranition-all hover:shadow-lg hover:-translate-y-px"}`}
+    className={`${!inGrid ? "mt-8 cursor-pointer group" : "flex flex-shrink-0 relative group cursor-pointer flex-col gap-2 h-48 print:h-min min-w-64 max-w-[90vw] w-96 print:w-min p-4 print:p-0 border print:border-none border-neutral-300 hover:border-neutral-400 dark:border-neutral-600 hover:dark:border-neutral-400 group rounded-lg tranition-all hover:shadow-lg hover:-translate-y-px"}`}
   >
     <Link
       href={`/blog/${post.slug}`}
@@ -32,7 +32,7 @@ const BlogListItem = ({
       <p>{post.description}</p>
       <DateAndLikes
         article={post}
-        className="sm:opacity-0 group-hover:opacity-100"
+        className="sm:opacity-0 group-hover:opacity-100 print:opacity-100"
       />
     </Link>
   </div>
