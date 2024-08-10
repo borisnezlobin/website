ad82f47a6bb8d61f5776ff316b7c51d39ed9463a
borisnezlobin <me@borisnezlobin.com>
2024-08-09 20:17:11 -0700

    horizontal scroll working

---
+++ app/components/landing/scroll-horizontal.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { LandingPageBadge } from "./landing-page-badge";
+
+type HorizontalScrollProps = {
+    children?: React.ReactNode[];
+}
+
+export const HorizontalScroll = ({ children }: HorizontalScrollProps) => {
+
+    return (
+        <div className="w-full overflow-hidden motion-reduce:overflow-auto relative scroller">
+            <div className="h-max py-4 flex flex-row motion-reduce:flex-wrap justify-start gap-4 items-center scroller-inner">
+                {children}
+                {children}
+            </div>
+        </div>
+    );
+}
+
+export default HorizontalScroll;
