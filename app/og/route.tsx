c40174818075d360feca2364ccb43f9dfd189cd3
borisnezlobin <me@borisnezlobin.com>
2024-08-11 19:41:40 -0700

    update og images to be good

--- app/og/route.tsx
+++ app/og/route.tsx
@@ -1,28 +1,44 @@
 import { ImageResponse } from 'next/og';
 import { NextRequest } from 'next/server';
- 
+
 export const runtime = 'edge';
- 
+
 export async function GET(request: NextRequest) {
     const params = request.nextUrl.searchParams;
 
     const subtitle = params.get("subtitle");
 
-    return new ImageResponse((
-        <div tw="w-full flex h-full bg-[#f5f5f5] p-16">
-            <div tw="mx-auto flex flex-col justify-center nowrap items-start">
-                <p tw='text-3xl text-[#707070]'>
-                    {params.get("info")}
-                </p>
-                <div tw="flex flex-col justify-center items-end">
-                    <p tw="text-8xl font-bold text-dark-background m-0" style={{}}>
-                        {params.get("title") || "Boris Nezlobin"}
-                    </p>
+    const fontUrl = `${request.nextUrl.origin}/Charter Regular.ttf`;
+    const fontResponse = await fetch(fontUrl);
+    const fontData = await fontResponse.arrayBuffer();
+    
+
+    return new ImageResponse(
+        (
+            <div tw="w-full flex h-full bg-[#f5f5f5] p-16">
+                <div tw="mx-auto flex flex-col justify-center items-center">
                     <p tw='text-3xl text-[#707070]'>
-                        {subtitle}{subtitle ? " / " : ""}{params.get("title") ? "Boris Nezlobin" : ""}
+                        {params.get("info")}
                     </p>
+                    <div tw="flex flex-col justify-center items-center">
+                        <p tw="text-8xl text-center font-bold text-[#1c1c1c] m-0 mb-2" style={{ fontFamily: 'Charter', lineHeight: "10rem" }}>
+                            {params.get("title") || "Boris Nezlobin."}
+                        </p>
+                        <p tw='text-3xl text-[#707070]' style={{ fontFamily: 'CustomFont' }}>
+                            {subtitle}{subtitle ? " / " : ""}{params.get("title") ? "Boris Nezlobin." : ""}
+                        </p>
+                    </div>
                 </div>
             </div>
-        </div>
-    ));
+        ),
+        {
+            fonts: [
+                {
+                    name: 'Charter',
+                    data: fontData,
+                    style: 'normal',
+                },
+            ],
+        }
+    );
 }
