065ad47f39938a7be8899e8267c08893604fdccf
borisnezlobin <boris.nezlobin@gmail.com>
2024-11-27 16:50:17 -0800

    add draft badges/handling for all routes so I can publish drafts

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -10,6 +10,7 @@ import { LinkButton } from "@/components/buttons";
 import BlogListItem from "../components/blog-list-item";
 import ArticleBody from "@/app/components/article-body";
 import { getBlog, getSimilarPosts } from "@/app/lib/db-caches";
+import { DraftBadge } from "@/app/components/draft-badge";
 
 export async function generateStaticParams() {
     const posts = await db.article.findMany({
@@ -71,59 +72,62 @@ export default async function SingleBlogPage({ params }: { params: { slug: strin
     }
     
     return (
-        <div className={`min-h-[100svh] print:min-h-0 z-[1] w-full p-8 print:bg-white print:text-dark-background print:dark:bg-white print:dark:text-dark-background`}>
-            {post.image && <ArticleImageBg imageUrl={post.image} />}
-            <header
-                className={`
-                    gap-3 rounded-lg backdrop-blur-lg z-[1] flex flex-col justify-start items-start p-0 
-                    ${post.image ? "mt-[24rem] md:mt-0 bg-light-background/30 dark:bg-dark-background/30 print:mt-0 md:items-center md:p-4" : "md:p-0 md:mt-8 max-w-2xl mx-auto"}
-                `}
-            >
-                <h1 className="text-3xl md:text-4xl bg-transparent dark:bg-transparent w-full text-center max-w-2xl text-[#101010] dark:text-[#fafafa] print:dark:text-[#101010]">
-                    {post.title}
-                </h1>
-                <p className={`bg-transparent dark:bg-transparent font-semibold print:text-left max-w-2xl ${!post.image ? "text-left mt-8" : "text-center"}`}>
-                    {post.description}
-                </p>
-            </header>
-            <div
-                className={`z-[1] w-full justify-center items-center relative mt-2 mb-8 p-0 md:p-8 rounded-lg ${!post.image ? "md:pt-0" : ""}`}
-            >
-                <div
-                    className={`z-[1] max-w-2xl ml-auto mr-auto relative w-full p-0 md:pt-8 rounded-lg`}
+        <>
+            {params.slug.startsWith("draft") && <DraftBadge />}
+            <div className={`min-h-[100svh] relative print:min-h-0 z-[1] w-full p-8 print:bg-white print:text-dark-background print:dark:bg-white print:dark:text-dark-background`}>
+                {post.image && <ArticleImageBg imageUrl={post.image} />}
+                <header
+                    className={`
+                        gap-3 rounded-lg backdrop-blur-lg z-[1] flex flex-col justify-start items-start p-0 
+                        ${post.image ? "mt-[24rem] md:mt-0 bg-light-background/30 dark:bg-dark-background/30 print:mt-0 md:items-center md:p-4" : "md:p-0 md:mt-8 max-w-2xl mx-auto"}
+                    `}
                 >
-                    <span className="text-muted dark:text-muted-dark font-normal">
-                        {new Date(post.createdAt).toLocaleDateString()}&nbsp;&nbsp;
-                    </span>
-                    <ArticleBody body={post.body} />
+                    <h1 className="text-3xl md:text-4xl bg-transparent dark:bg-transparent w-full text-center max-w-2xl text-[#101010] dark:text-[#fafafa] print:dark:text-[#101010]">
+                        {post.title}
+                    </h1>
+                    <p className={`bg-transparent dark:bg-transparent font-semibold print:text-left max-w-2xl ${!post.image ? "text-left mt-8" : "text-center"}`}>
+                        {post.description}
+                    </p>
+                </header>
+                <div
+                    className={`z-[1] w-full justify-center items-center relative mt-2 mb-8 p-0 md:p-8 rounded-lg ${!post.image ? "md:pt-0" : ""}`}
+                >
+                    <div
+                        className={`z-[1] max-w-2xl ml-auto mr-auto relative w-full p-0 md:pt-8 rounded-lg`}
+                    >
+                        <span className="text-muted dark:text-muted-dark font-normal">
+                            {new Date(post.createdAt).toLocaleDateString()}&nbsp;&nbsp;
+                        </span>
+                        <ArticleBody body={post.body} />
+                    </div>
                 </div>
-            </div>
-            {post && (
-                <div className="z-[1] flex flex-row justify-start items-center gap-2 print:hidden">
-                    <p className="text-muted dark:text-muted-dark">Liked this article?</p>
-                    <ShareButton />
-                    <TweetArticleButton slug={post.slug} />
-                </div>
-            )}
-            <LinkButton
-                direction="left"
-                aria-label="Back to Blog"
-                className="mt-8 print:hidden"
-                href="/blog"
-            >
-                Back to blog
-            </LinkButton>
+                {post && (
+                    <div className="z-[1] flex flex-row justify-start items-center gap-2 print:hidden">
+                        <p className="text-muted dark:text-muted-dark">Liked this article?</p>
+                        <ShareButton />
+                        <TweetArticleButton slug={post.slug} />
+                    </div>
+                )}
+                <LinkButton
+                    direction="left"
+                    aria-label="Back to Blog"
+                    className="mt-8 print:hidden"
+                    href="/blog"
+                >
+                    Back to blog
+                </LinkButton>
 
-            <h2 className="text-2xl mt-12 print:hidden">More articles</h2>
-            <div className="md:pl-8 print:hidden">
-                <ul>
-                    {similarPosts.map((post) => (
-                        <li key={post.id}>
-                            <BlogListItem post={post} inGrid={false} />
-                        </li>
-                    ))}
-                </ul>
+                <h2 className="text-2xl mt-12 print:hidden">More articles</h2>
+                <div className="md:pl-8 print:hidden">
+                    <ul>
+                        {similarPosts.map((post) => (
+                            <li key={post.id}>
+                                <BlogListItem post={post} inGrid={false} />
+                            </li>
+                        ))}
+                    </ul>
+                </div>
             </div>
-        </div>
+        </>
     );
 }
