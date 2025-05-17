d523a93d6e7448598f6a0c0ac47c5b3092f4fbaf
borisnezlobin <boris.nezlobin@gmail.com>
2025-05-17 10:01:49 -0700

    notes now all-html yippee

--- app/notes/[slug]/page.tsx
+++ app/notes/[slug]/page.tsx
@@ -1,11 +1,11 @@
 import db from "@/app/lib/db";
 import getMetadata from "@/app/lib/metadata";
-import { getNoteSections } from "../getNoteSections";
+import { getNoteSections, getHTMLNoteSections } from "../getNoteSections";
 import { readFileSync } from "fs";
 import Link from "next/link";
 import NotFoundPage from "@/app/components/not-found-page";
 import BackToRouteLink from "@/app/components/back-to-route";
-import getNoteMdxPath from "@/app/utils/get-note-mdx-path";
+import getNoteMdxPath, { getNoteHTMLPath } from "@/app/utils/get-note-mdx-path";
 import { getNote, getNotes } from "@/app/lib/db-caches";
 
 export async function generateMetadata({ params }: { params: { slug: string } }) {
@@ -43,9 +43,9 @@ export default async function SubjectNotesPage({ params }: { params: { slug: str
         )
     }
 
-    const configDirectory = getNoteMdxPath(note.slug);
+    const configDirectory = getNoteHTMLPath(note.slug);
     const content = readFileSync(configDirectory, "utf-8"); // TODO: make this cached
-    const sections = getNoteSections(content);
+    const sections = getHTMLNoteSections(content);
 
     const numWords = content.split(/\s+/).length;
 
@@ -53,16 +53,16 @@ export default async function SubjectNotesPage({ params }: { params: { slug: str
         <div className="min-h-[100svh] print:min-h-0 dark:bg-dark-background z-[1] w-full p-8 md:pt-8 text-light-foreground dark:text-dark-foreground">
             <BackToRouteLink href="/notes" text="Back to Notes" />
             <h1 className="text-3xl font-bold emph pt-4">{note.title}</h1>
-            <p className="mt-6 text-muted dark:text-muted-dark">Roughly {numWords} words.</p>
-            <p>{note.description}</p>
+            {/* <p className="mt-6 text-muted dark:text-muted-dark">Roughly {numWords} words.</p> */}
+            <p className="mt-6">{note.description}</p>
             <div className="h-px w-full bg-muted dark:bg-muted-dark my-8" />
             <h2 className="text-2xl font-bold">Table of Contents</h2>
-            <ol>
+            <ol className="pl-8">
                 {sections.map((section, i) => (
                     <div key={section.slug}>
                         <Link className="link" href={`/notes/${note.slug}/${section.slug}`}>
                             <li className="mt-4">
-                                {i + 1}. <span className="text-lg font-bold">{section.title}</span>
+                                <span className="text-lg font-bold">{section.title}</span>
                             </li>
                         </Link>
                     </div>
