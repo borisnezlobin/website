912367091360acfa29de8d9b0ae0df0624176d04
borisnezlobin <boris.nezlobin@gmail.com>
2025-06-16 15:18:43 -0700

    minor fixes

--- app/blog/components/article-date.tsx
+++ app/blog/components/article-date.tsx
@@ -1,10 +1,11 @@
+import { formatDateWithOrdinal } from "@/app/utils/format-date";
 import { Article } from "@prisma/client";
 
 const ArticleDate = ({ article, className, containerClass }: { article: Article, className?: string, containerClass?: string }) => {
     return (
         <div className={"flex justify-between " + containerClass}>
             <time className={`${!className || !className.includes("text") ? "text-muted dark:text-muted-dark" : ""} ${className}`}>
-                {new Date(article.createdAt).toLocaleDateString()}
+                {formatDateWithOrdinal(new Date(article.createdAt))}
             </time>
         </div>
     )
