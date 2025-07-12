f487a38e95d7c30649ed6ae9ca8452493927030d
borisnezlobin <boris.nezlobin@gmail.com>
2025-07-12 02:30:55 -0700

    source code pro out; replaced with courier new

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
             <head>
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" />
-                <link href="https://fonts.googleapis.com/css2?family=Source+Code+Pro:ital,wght@0,200..900;1,200..900&display=swap" rel="stylesheet" />
+                {/* <link href="https://fonts.googleapis.com/css2?family=Source+Code+Pro:ital,wght@0,200..900;1,200..900&display=swap" rel="stylesheet" /> */}
             </head>
             <body suppressHydrationWarning>
                 <ThemeProvider attribute="class">
