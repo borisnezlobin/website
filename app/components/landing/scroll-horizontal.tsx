71626b1545517003f8144abae2450b50a4d13995
borisnezlobin <me@borisnezlobin.com>
2025-08-09 17:32:20 -0700

    they stopped giving out most printable website of the year after I won so many. also fix landing page

--- app/components/landing/scroll-horizontal.tsx
+++ app/components/landing/scroll-horizontal.tsx
@@ -2,9 +2,10 @@ import { Children, cloneElement, isValidElement, ReactElement, ReactNode } from 
 
 interface HorizontalScrollProps {
     children: ReactNode;
+    className?: string;
 }
 
-const HorizontalScroll = ({ children }: HorizontalScrollProps) => {
+const HorizontalScroll = ({ children, className = "" }: HorizontalScrollProps) => {
     // Ensure children is an array of ReactElement
     const elements = Children.toArray(children).filter(
         (child): child is ReactElement => isValidElement(child)
@@ -12,7 +13,7 @@ const HorizontalScroll = ({ children }: HorizontalScrollProps) => {
 
     return (
         <>
-            <div className="print:hidden w-full overflow-hidden motion-reduce:overflow-auto print:overflow-auto relative scroller">
+            <div className={`print:hidden w-full overflow-hidden motion-reduce:overflow-auto print:overflow-auto relative scroller ${className}`}>
                 <div className="h-max py-4 flex flex-row print:flex-col motion-reduce:flex-wrap print:flex-wrap justify-start gap-4 items-center scroller-inner">
                     {children}
                     {elements.map((e, i) => {
