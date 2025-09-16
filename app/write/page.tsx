481b6dc5bd2c2efdd5ce9aa31d82bce0f73c15b5
borisnezlobin <boris.nezlobin@gmail.com>
2025-09-16 15:05:48 -0700

    rebrand

--- app/write/page.tsx
+++ app/write/page.tsx
@@ -3,7 +3,8 @@ import getMetadata from "../lib/metadata"
 import WritePageComponent from "./write-component"
 
 export const metadata: Metadata = getMetadata({
-    title: "Write Now",
+    title: "Wrisk",
+    subtitle: "Don't stop writing or you lose it all.",
     description: "Set a time, give yourself a prompt, and write. No distractions and definitely no stopping. Pause for too long and you'll lose everything :)"
 });
 
