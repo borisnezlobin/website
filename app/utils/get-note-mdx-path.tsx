6739c237665e1395198a1b7b6f6d91c38b110fa3
borisnezlobin <me@borisnezlobin.com>
2025-10-12 17:18:34 -0700

    remove stuff and make blog read from remotes

--- app/utils/get-note-mdx-path.tsx
+++ app/utils/get-note-mdx-path.tsx
@@ -1,19 +1,19 @@
 import path from "path";
 
 const getNoteMdxPath = (slug: string) => {
-    return path.resolve(process.cwd(), path.join("mdx", "notes", slug.replaceAll("draft-", "") + ".mdx"));
+    return path.resolve(process.cwd(), path.join("html", "notes", slug.replaceAll("draft-", "") + ".mdx"));
 }
 
 const getNoteHTMLPath = (slug: string) => {
-    return path.resolve(process.cwd(), path.join("mdx", "notes", slug.replaceAll("draft-", "") + ".html"));
+    return path.resolve(process.cwd(), path.join("html", "notes", slug.replaceAll("draft-", "") + ".html"));
 }
 
 const getBlogHTMLPath = (slug: string) => {
-    return path.resolve(process.cwd(), path.join("mdx", "blog", slug.replaceAll("draft-", "") + ".html"));
+    return path.resolve(process.cwd(), path.join("html", "blog", slug.replaceAll("draft-", "") + ".html"));
 }
 
 const getProjectHTMLPath = (slug: string) => {
-    return path.resolve(process.cwd(), path.join("mdx", "project", slug.replaceAll("draft-", "") + ".html"));
+    return path.resolve(process.cwd(), path.join("html", "project", slug.replaceAll("draft-", "") + ".html"));
 }
 
 export default getNoteMdxPath;
