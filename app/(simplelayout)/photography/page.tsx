393099aff98cdef4b32f361a722608df0c6760f6
borisnezlobin <me@borisnezlobin.com>
2025-12-19 00:02:00 -0800

    make things slightly more readable

--- app/(simplelayout)/photography/page.tsx
+++ app/(simplelayout)/photography/page.tsx
@@ -11,10 +11,12 @@ export const metadata: Metadata = getMetadata({
 
 export default async function PhotographyPage() {
     const photos = await getPhotographs();
+
+
     
     return (
         <div className="w-full flex flex-row flex-wrap justify-center items-left mt-4 print:m-0 gap-4">
-            <div className="w-full text-left max-w-5xl mx-auto text-3xl font-bold emph">
+            {/* <div className="w-full text-left max-w-5xl mx-auto text-3xl font-bold emph">
                 My Photography
             </div>
             <div className="w-full flex flex-row flex-wrap justify-center items-left print:m-0 gap-4">
@@ -22,8 +24,15 @@ export default async function PhotographyPage() {
                     All shot on an iPhone 16 Pro. These are mostly unedited and shot with default Night Mode settings, but some of the more interesting photos use custom shutter speeds/exposure/filters.
                     You can&apos;t undo liking one of the pictures.
                 </p>
+            </div> */}
+            {/* {photos.map((photo) => <PhotographItem key={photo.id} photo={photo} />)} */}
+
+            {/* only show the first image */}
+            <div className="w-full h-screen flex flex-row flex-wrap justify-center items-left print:m-0 gap-4">
+                <div className="w-full h-screen max-w-5xl mx-auto">
+                    <img src={photos[3].image} alt={photos[0].title} className="h-full w-auto mx-auto" />
+                </div>
             </div>
-            {photos.map((photo) => <PhotographItem key={photo.id} photo={photo} />)}
         </div>
     );
 }
