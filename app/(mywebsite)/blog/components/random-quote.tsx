152f44139d6819d9f417361975958968edc12bea
borisnezlobin <me@borisnezlobin.com>
2026-03-19 02:06:41 -0700

    perhaps the coolest effect I've made in a while... great transition between technical and creative writing. does not work on mobile

--- app/(mywebsite)/blog/components/random-quote.tsx
+++ app/(mywebsite)/blog/components/random-quote.tsx
@@ -145,7 +145,7 @@ const RandomQuote: React.FC<{ visible?: boolean }> = ({ visible }) => {
                 <p className="text-muted-dark dark:text-muted pl-2 whitespace-nowrap emph absolute bottom-0 left-0 w-full text-center py-2 overflow-hidden text-ellipsis">
                     {quotes[index].source}
                 </p>
-                <InkscapeColoredSvg strokeWidth={0.5} path="/drawings/cornerborder.svg" color="#1A1714" className="absolute h-full top-[-57.2%] right-[-15.8%]" visible={visible} />
+                <InkscapeColoredSvg strokeWidth={0.5} path="/drawings/cornerborder.svg" color="var(--text-color)" className="absolute h-full top-[-52.4%] right-[-14%]" visible={visible} />
             </center>
             <div className="w-full flex items-center justify-center mb-6">
                 <p
